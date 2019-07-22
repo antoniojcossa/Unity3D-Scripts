f7bfa33aa90c88d0020883acd2f4dc6d714a6d91
AndersonQ306 <32402003+AndersonQ306@users.noreply.github.com>
2019-07-22 15:22:17 +0200

    Add files via upload

---
+++ SpinningCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningCube : MonoBehaviour {
+	//Simply attach this to a gameObject and you're good to go!
+    //You can adjust the rotation values in the Unity Editor
+    [SerializeField] private float xAxisSpeed = 50f; //X Axis Rotation Speed
+    [SerializeField] private float yAxisSpeed = 50f; //Y Axis Rotation Speed
+    [SerializeField] private float zAxisSpeed = 50f; //Z Axis Rotation Speed
+
+    void Update () {
+        transform.Rotate(Vector3.up, xAxisSpeed * Time.deltaTime); //Rotate from the Y axis
+        transform.Rotate(Vector3.left, yAxisSpeed * Time.deltaTime); //Rotate from the X axis
+        transform.Rotate(Vector3.forward, zAxisSpeed * Time.deltaTime); //Rotate from the Z axis
+    }
+}
